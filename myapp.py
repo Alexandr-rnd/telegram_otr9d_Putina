978629cf449c9cac3848a9201999209d89aa2935
Alexandr-rnd <80747786+Alexandr-rnd@users.noreply.github.com>
2021-03-19 16:22:03 +0300

    Add files via upload

---
+++ myapp.py
@@ -0,0 +1,26 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+from config import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+
+
+@dp.message_handler(commands=['start1'])
+async def alarm(message: types.Message):
+    keyboard_markup = types.InlineKeyboardMarkup()
+    user_id_btn = types.InlineKeyboardButton('Получить ID пользывателя из Inline кнопки', callback_data='user_id')
+    keyboard_markup.row(user_id_btn)
+    await message.answer(f"Ваш ID: {message.from_user.id}", reply_markup=keyboard_markup)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'user_id')
+async def user_id_inline_callback(callback_query: types.CallbackQuery):
+    await callback_query.answer(f"Ваш ID: {callback_query.from_user.id}", True)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
---
+++ myconfig.py
@@ -0,0 +1,2 @@
+BOT_TOKEN = '1645697487:AAGwFMe_Ybuo0rY3W4D6xlPlC0_gjlcAfBc'
+admin_id = ''
